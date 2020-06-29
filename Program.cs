15a438ee2a12e6f5dd2e640256830670c678317c
crystal <crystal@coastal.edu>
2020-06-29 16:13:48 -0400

    Added another Hello World

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Library
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
